8b1a64e962cc92047d243a25a1a3374805021f16
Justin Hope <jhope@q30design.com>
2018-10-11 11:06:10 -0400

    updated contact page

---
+++ contact/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$page_name = 'Contact';
+include('../includes/header.inc.php');
+
+?>
+
+<main>
+
+<section>
+    <div class="gh-hero gh-hero-full">
+        <span class="gh-caption">
+            <h1>Contact us</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </span>
+    </div>
+</section>
+
+</main>
+
+<?php include('../includes/footer.inc.php'); ?>
